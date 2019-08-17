611ec9e419b9a6dde3697023ccb92af144406792
Arun Kolhapur <arun.kolhapur@verse.in>
2019-08-17 22:33:41 +0530

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "./partials/layout/head.html";?>
+
+<body>
+    <header id="header-wrapper" class="container-fluid bg-dark">
+        <?php include "./partials/layout/header.html";?>
+    </header>
+
+    <div id="content-wrapper" class="container rounded">
+        <?php include "./partials/homepage.php";?>
+    </div>
+
+    <footer>
+        <?php include "./partials/layout/footer.html";?>
+    </footer>
+</body>
+
+</html>
---
+++ partials/about.php
@@ -0,0 +1,47 @@
+<div id="about-wrapper" class="">
+    <legend>About Us - Who are we?</legend>
+    <div class="row pt-3">
+        <img src="assets/images/about-us2.jpg" alt="" class="col-12">
+    </div>
+
+    <div class="container p-3">
+        <div class="row pt-2">
+            <p>
+                Symode Consulting encompasses a dedicated team of professionals who efficiently partner with the
+                companies by providing comprehensive recruitment and market/competitive intelligence services.
+                Our recruitment services help companies expand their business potential by building a skilled and
+                efficient workforce, and the market/competitive intelligence services coupled with our multi-industry
+                business and technical expertise built over a decade of experience enables us to deliver insights that
+                are relevant and crucial for taking informed decisions and developing effective business strategies.
+            </p>
+
+            <h3>Recruitment services:</h3>
+            <p>
+                We at Symode understand the increasing and complex demands of today’s new age enterprises in
+                terms of finding relevant talent who are very passionate and dedicated to achieving the business
+                objectives. We firmly believe that skilled and efficient employees are the major pillars of any
+                organization. With this focus, we cater to corporate clients across different industries, providing
+                them with efficient manpower for their manifold recruitment requirements. Among the plethora of
+                skilled professionals available across the country, we follow a multi-level recruitment process to
+                ensure more effective and successful workforce delivery by understanding the specific requirements
+                of clients and the candidates.
+            </p>
+
+            <h3>Market/Competitive Intelligence services: </h3>
+            <p>
+                Our strength also lies in providing relevant and useful market/competition-related insights to the
+                clients with a wide range of offerings which can help the companies understand the overall
+                landscape of the business that they are operating in. Our team of highly skilled industry experts and
+                analysts focus on emerging market trends across different industries to help clients devise effective
+                action plans and market strategies to optimize their market position while building a future-proof
+                business strategy. We always try to find innovative ways to serve our best to the clients and make a
+                positive difference. Firms of all sizes will benefit from our partnership with relevant market and
+                competitive insights to drive higher growth, faster customer acquisition, and a sustainable edge in
+                their business.
+            </p>
+
+            Our company is strategically based out of Bangalore, which is one of the strongest &amp; broadest hubs
+            for both talent and technology in India.
+        </div>
+    </div>
+</div>
